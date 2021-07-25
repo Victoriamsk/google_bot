ca8fbc54d836e31573fbe9af1e0fc53dc2726f6b
Victoria <vikamac@yahoo.com>
2021-07-25 21:19:44 +0300

    Create googleBot.js
    Программа для выбора случайного ключевого слова и поиска сайта "музыкалка-онлайн .рф'  по выбранному ключевому слову.
    После нахождения сайта программа переходит на данный сайт.
    
    A program for selecting a random keyword and searching for the site "музыкалка-онлайн .рф ' by the selected keyword. 
    After finding a site, the program goes to this site.

---
+++ googleBot.js
@@ -0,0 +1,42 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.google.com/*
+// @icon         https://www.google.com/s2/favicons?domain=tampermonkey.net
+// @grant        none
+// ==/UserScript==
+
+let words = ['Гобой', 'Флейта', 'Как звучит кларнет', 'Валторна', 'Фагот'];
+let googleInput = document.getElementsByName('q') [0];
+googleInput.value = words[getIntRandom(0, words.length)];
+
+let btnK = document.getElementsByName('btnK') [0];
+if(btnK != undefined) {
+    setTimeout(function() {
+    btnK.click();
+
+    }, 1500);
+} else {
+    let links = document.links;
+    let pnnext = document.getElementById('pnnext');
+    let goToTheNextPage = true;
+    for(let i = 0; i < links.length; i++) {
+        let link = links[i];
+        if(link.href.indexOf('xn----7sbab5aqcbiddtdj1e1g.xn--p1ai') != -1) {
+            setTimeout(function() {
+                link.click();
+            }, 2000);
+            goToTheNextPage = false;
+            break;
+        }
+    }
+    if(goToTheNextPage) setTimeout(function(){pnnext.click();}, 1800);
+}
+
+function getIntRandom (min, max) {
+    return Math.floor(Math.random()*(max-min)+min);
+}
+
